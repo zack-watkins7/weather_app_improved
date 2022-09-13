3013445d222a9ebc776df9ce30a490cb3c1ba8ee
Zachary Watkins <zwatkins297@gmail.com>
2022-09-12 22:50:42 -0700

    updated the javascript

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-let weather = {
-    API_key: "60aa29f93c1f36ae13b3256561abe297",
-    getWeather: function (city) {
-        fetch(
-            "http://api.openweathermap.org/data/2.5/weather?q="
-            + city 
-            +",uk&APPID="
-            + this.API_key
-        )
-            .then((response) => response.json())
-            .then((data) => console.log(data));
-    },
-    displayWeather: function(data) {
+// https://api.openweathermap.org/data/2.5/weather?q=Denver&appid=60aa29f93c1f36ae13b3256561abe297
 
+let weather = {
+    API_key : '60aa29f93c1f36ae13b3256561abe297',
+    fetchWeather : function (city){
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid="+ this.API_key
+        ).then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data){
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, icon, description, temp, humidity, speed);
+    document.querySelector(".city_name").innerText= "The Weather in "+ name;
     }
 };
